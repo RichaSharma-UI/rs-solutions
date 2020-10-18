6b6581e370622ba1c478a61c24cbc24c177891ff
richasharma-ui <aim.richa.sharma@gmail.com>
2020-10-18 16:46:33 +0530

    RSSolution-2: Added register page

---
+++ src/Services/UserService.js
@@ -0,0 +1,27 @@
+import { API_BASE_URL } from './Constants';
+
+export const userService = {
+    register
+};
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${API_BASE_URL}/users/register`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
---
+++ src/State/Actions/UserActions.js
@@ -0,0 +1,26 @@
+import { userConstants } from '../Constants';
+import { userService } from '../../Services';
+
+export const userActions = {
+    register
+};
+
+function register(user) {
+    return dispatch => {
+        dispatch(request(user));
+
+        userService.register(user)
+            .then(
+                user => { 
+                    dispatch(success());
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                }
+            );
+    };
+
+    function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
+    function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
+    function failure(error) { return { type: userConstants.REGISTER_FAILURE, error } }
+}
